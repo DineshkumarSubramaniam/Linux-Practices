e4962664bfd65066d1afa2da6843c4cc77a7caec
Dineshkumar Subramaniam <113435454+DineshkumarSubramaniam@users.noreply.github.com>
2022-12-18 22:01:22 -0800

    Add files via upload

---
+++ nice3.c
@@ -0,0 +1,58 @@
+/* Program to demonstrate nice command in Linux*/
+
+// Inclusion of header file
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+
+int main(int argc , char*argv[])
+{
+       // condition to check the required argument is passed or not
+       if(argc != 3)
+        {
+          printf("give the process id and nice value\n");
+          return 1;
+        }
+        
+        // dec of variables
+        int id, priority,ret,nice;
+        
+        // converting string to integer
+        // process id
+        id = atoi(argv[1]);
+        // nice value
+        nice= atoi(argv[2]);
+    
+    // getting the priority values
+    priority = getpriority(PRIO_PROCESS, id);
+    
+    // set priority and saving in a variable
+    ret = setpriority(PRIO_PROCESS, id,nice);
+
+    // condition to print the set priority
+    if (ret == 0)
+    {
+        printf("priority is set\n");
+        priority = getpriority(PRIO_PROCESS, id);
+        printf("new priority : %d\n",priority);
+    }
+    // condition to print if priority set is not found 
+    else
+    {
+        printf("not set\n");
+    }   
+}
+
+
+
+/*
+PRIO_PROCESS
+indicates that the who argument is to be interpreted as a process ID
+
+PRIO_PGRP
+indicates that the who argument is to be interpreted as a process group ID
+
+PRIO_USER
+indicates that the who argument is to be interpreted as a user ID
+*/
